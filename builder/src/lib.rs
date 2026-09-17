11fd5097c3f693db4bda03fde6a4a58f566ff138
Callum Adair <callum.adair@outlook.com>
2026-09-18 00:53:21 +0100

    progress on optional fields

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -6,6 +6,12 @@ use syn::{
     Attribute,
     Data,
     DeriveInput,
+    GenericArgument,
+    Ident,
+    Path,
+    PathArguments,
+    Type,
+    TypePath,
     parse_macro_input,
 };
 
@@ -80,32 +86,30 @@ fn create_builder_fields_and_methods(
         {
             match data_struct.fields
             {
-                syn::Fields::Named(fields_named) => fields_named
-                    .named
-                    .iter()
-                    .map(|field| {
-                        let field_ident = field.ident.clone().unwrap();
-                        let field_ty = field.ty.clone();
-                        (
-                            quote! {
-                                #field_ident
-                            },
-                            quote! {
-                              #field_ty
-                            },
-                            quote! {
-                                fn #field_ident(&mut self, #field_ident: #field_ty) -> &mut Self {
-                                    self.#field_ident = Some(#field_ident);
-                                    self
-                                }
-                            },
-                        )
-                    })
-                    .collect::<(
-                        Vec<proc_macro2::TokenStream>,
-                        Vec<proc_macro2::TokenStream>,
-                        Vec<proc_macro2::TokenStream>,
-                    )>(),
+                syn::Fields::Named(fields_named) =>
+                {
+                    fields_named
+                        .named
+                        .iter()
+                        .map(|field| {
+                            let field_ident = field.ident.clone().unwrap();
+                            let field_ty = field.ty.clone();
+                            (
+                                quote! {
+                                    #field_ident
+                                },
+                                quote! {
+                                  #field_ty
+                                },
+                                construct_builder_method(&field_ident, &field_ty),
+                            )
+                        })
+                        .collect::<(
+                            Vec<proc_macro2::TokenStream>,
+                            Vec<proc_macro2::TokenStream>,
+                            Vec<proc_macro2::TokenStream>,
+                        )>()
+                }
                 syn::Fields::Unnamed(_) | syn::Fields::Unit =>
                 {
                     unimplemented!("We only support structs with named fields.")
@@ -117,4 +121,97 @@ fn create_builder_fields_and_methods(
             unimplemented!("We do not support a builder macro for any type other than a struct.")
         }
     }
+}
+
+fn construct_builder_method(
+    field_ident: &Ident,
+    field_type: &Type,
+) -> proc_macro2::TokenStream
+{
+    match extract_field_type_kind(field_type)
+    {
+        FieldKind::Field(ident) =>
+        {
+            quote! {
+                fn #field_ident(&mut self, #field_ident: #ident) -> &mut Self {
+                    self.#field_ident = Some(#field_ident);
+                    self
+                }
+            }
+        }
+        FieldKind::OptionalField(ident) =>
+        {
+            quote! {
+                fn #field_ident(&mut self, #field_ident: #ident) -> &mut Self {
+                    self.#field_ident = Some(Some(#field_ident));
+                    self
+                }
+            }
+        }
+    }
+}
+
+enum FieldKind
+{
+    Field(Ident),
+    OptionalField(Ident),
+}
+
+fn extract_field_type_kind(ty: &Type) -> FieldKind
+{
+    let Type::Path(TypePath {
+        attrs: _,
+        qself: _,
+        path: Path {
+            leading_colon: _,
+            segments: outer_segments,
+        },
+    }) = ty
+    else
+    {
+        unimplemented!("We do not support types other than Path based types.")
+    };
+
+    let Some(outer_segment) = outer_segments.last()
+    else
+    {
+        unimplemented!("Bad types provided in struct.")
+    };
+
+    if outer_segment.ident != "Option"
+    {
+        return FieldKind::Field(outer_segment.ident.clone());
+    }
+
+    let PathArguments::AngleBracketed(angle_bracketed) = outer_segment.arguments.clone()
+    else
+    {
+        unimplemented!("We don't do this case currently.");
+    };
+    let Some(GenericArgument::Type(inner_type)) = angle_bracketed.args.first()
+    else
+    {
+        unimplemented!("We were kinda expecting a generic argument containing an inner type here.")
+    };
+
+    let Type::Path(TypePath {
+        attrs: _,
+        qself: _,
+        path:
+            Path {
+                leading_colon: _,
+                segments: inner_type_segments,
+            },
+    }) = inner_type
+    else
+    {
+        unimplemented!("We do not support types other than Path based types.")
+    };
+
+    let Some(inner_segment) = inner_type_segments.last()
+    else
+    {
+        unimplemented!("Bad types provided in struct.")
+    };
+    FieldKind::OptionalField(inner_segment.ident.clone())
 }
